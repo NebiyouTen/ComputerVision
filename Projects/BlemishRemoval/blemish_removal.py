7b6f91a1acefbe09fc885de990eabbb822f7a21a
NebiyouTen <nebiyoutenager@gmail.com>
2019-10-03 21:57:20 -0600

    Finalized blemish removal

--- Projects/BlemishRemoval/blemish_removal.py
+++ Projects/BlemishRemoval/blemish_removal.py
@@ -1,7 +1,16 @@
 '''
-    Blemish removal
+    Blemish removal using OpenCV
 
     Nebiyou Yismaw
+
+    This program will allow the user to click on a point of an image. It has a
+    specified radius and will look for a patch of images centered at point +/-
+    the radius. These points are found using the patch_inds list. To find each
+    path every element in the patch_inds list will be used. It's found using the
+    equation center = (x,y) + i * (radius), where i is each element in the patch_inds
+    list. After finding these patches, Fourier transform is used to find the
+    smoothest patch. Once it's found, it will be copied to our original image
+     using seamless cloning.
 
 '''
 
@@ -16,37 +25,42 @@ np.set_printoptions(threshold=sys.maxsize)
 matplotlib.rcParams['figure.figsize'] = (6.0, 6.0)
 matplotlib.rcParams['image.cmap'] = 'gray'
 
-# coordiante for patches relative to belmish
+# coordiante for patches relative to belmish, these are realtive to the selected
+# point and will be scaled by our radius
 patch_inds = [[0, -1], [1, -1], [1, 0],
               [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]]
 
 
 def compute_ft(image):
     '''
-        Compute FT
+        Compute FT. This function will compute the fourier transform of our image
     '''
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    dft = cv2.dft(np.float32(image_gray),flags = cv2.DFT_COMPLEX_OUTPUT)
+    dft = cv2.dft(np.float32(image_gray), flags=cv2.DFT_COMPLEX_OUTPUT)
     dft_shift = np.fft.fftshift(dft)
-    magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+    magnitude_spectrum = 20 * \
+        np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
     return magnitude_spectrum
-
 
 
 def get_slice(x, a, radius):
     '''
-        A fuction to return slice given a point
+        A fuction to return slice given a point, a direction (slope) and a radius
     '''
-    center = x + 2 * a * radius
+    center = x + a * radius
     return slice(center - radius, center + radius)
 
 
 def get_patches(x, y, radius, image):
     '''
-        A function to return list of patches
+        A function to return a list of patches. Given a specific pixel this
+        function will return a list of patches around that point. For each
+        element in the patch indices, it will append the corresponding patch
+        and append it to our list. Finally returns the list.
     '''
     patches = []
-    patches.extend( image[get_slice(x, j, radius), get_slice(y, i, radius), :] for j, i in patch_inds )
+    patches.extend(image[get_slice(x, j, radius), get_slice(
+        y, i, radius), :] for j, i in patch_inds)
     return patches
 
 
@@ -55,25 +69,31 @@ def callback(action, y, x, flags, userdata):
         Mouse event callback function
     '''
     global src_img
-    radius = 15
+    # define a radious around our given point
+    radius = 14
+    # if mouse button is pressed
     if action == cv2.EVENT_LBUTTONDOWN:
-        print (x,y)
-        # print(x, y)
+        # get all the candidate patches
         pathces = get_patches(x, y, radius, userdata)
-        orig = compute_ft(userdata[ x-radius:x+radius, y-radius : y+radius,: ])
-        print ("orig ", y,x, orig.mean())
-        spec_means = np.array([ compute_ft(patch).mean() for patch in pathces  ])
-        best_patch = pathces[ spec_means.argmin() ]
-        if orig.mean() > spec_means.mean().min():
+        orig = compute_ft(
+            userdata[x - radius:x + radius, y - radius: y + radius, :])
+        spec_means = np.array([compute_ft(patch).mean() for patch in pathces])
+        best_patch = pathces[spec_means.argmin()]
+        if orig.mean() > spec_means.min():
             # best_patch = np.zeros_like(best_patch)
-            print ("Means are ", spec_means)
-            src_mask = np.ones(best_patch.shape, best_patch.dtype) * 255
+            mask = 255 * np.ones(best_patch.shape, best_patch.dtype)
             # img = userdata
-            src_img = cv2.seamlessClone(best_patch, userdata, src_mask, (y, x), cv2.MIXED_CLONE)
+            __before__ = np.copy(src_img)
+            # src_img[x-radius:x+radius,y-radius:y+radius,:] = best_patch
+            src_img = cv2.seamlessClone(
+                best_patch, src_img, mask, (y, x), cv2.NORMAL_CLONE)
+            # show image
+            __after__ = np.copy(src_img)
+            cv2.imshow("Window", src_img)
 
-            print ("Data:::: ",best_patch.shape, userdata.shape, src_mask.shape, (y,x))
 
 src_img = []
+
 
 def main(argv):
     '''
@@ -86,13 +106,15 @@ def main(argv):
     # let's copy this iamge
     src_img_cpy = src_img.copy()
     # crete a named window
-    cv2.namedWindow("Window")
+    cv2.namedWindow("Window", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Window', 720, 720)
     # let's add mouse event handler for our named window
     cv2.setMouseCallback("Window", callback, src_img)
     # exit if esc key is pressed
+    # show image
+    print(" Starting mean is ", src_img.mean())
     k = 0
     while k != 27:
-        # show image
         cv2.imshow("Window", src_img)
         cv2.putText(src_img, 'Press ESC to exit and c to clear',
                     (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
