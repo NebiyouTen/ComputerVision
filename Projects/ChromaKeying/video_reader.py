c43cd54e29d0231ed9f1b0b91378d35f1fbf579a
NebiyouTen <nebiyoutenager@gmail.com>
2019-10-09 23:01:30 -0600

    Adding a script to read a video file and write the first frame to a file, to help chromkeying algorithm

---
+++ Projects/ChromaKeying/video_reader.py
@@ -0,0 +1,62 @@
+'''
+    Author: Nebiyou Yismaw
+
+    This is a python code that will that will use OPenCV and read and display
+    a video.
+
+'''
+# import requried libraries
+import sys
+import cv2
+import numpy as np
+np.set_printoptions(threshold=sys.maxsize)
+import matplotlib.pyplot as plt
+# configure matplot lib
+import matplotlib
+matplotlib.rcParams['figure.figsize'] = (6.0, 6.0)
+matplotlib.rcParams['image.cmap'] = 'gray'
+
+def print_video_properties(cap):
+    '''
+        This function will get video properties from a capture object and
+        prints them.
+    '''
+    # get width of frames the video
+    width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    print ("Video resolution is %dx%d" %(width, height))
+
+'''
+    @main
+'''
+def main(argv):
+    # create a video capture object by passing a file name
+    cap = cv2.VideoCapture('greenscreen-demo.mp4')
+    # Check if camera opened successfully
+    if (cap.isOpened()== False):
+        print("Error opening video stream or file")
+    else: # if capture object is opened successfully
+        # print video properties
+        print_video_properties(cap)
+        # while capture object is opened
+        while(cap.isOpened()):
+          # Capture frame-by-frame
+          ret, frame = cap.read()
+          if ret == True:
+              # Display the resulting frame
+              cv2.imshow('Frame',frame)
+              cv2.imwrite("greenscreen-demo.jpg", frame)
+              # Press esc on keyboard to  exit
+              # wait take time to wait before timing out in ms. Here we wait for
+              # 25ms before displaying the next frame
+              if cv2.waitKey(25) & 0xFF == 27:
+                  break
+
+          else:
+              break
+          break
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
